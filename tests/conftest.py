c6284d4dd9553fac6016c86ba415475b5424e20d
David Sanders <davesque@gmail.com>
2019-07-04 00:22:38 -0600

    Take better advantage of pytest's API

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,13 +12,13 @@ FIXTURES = list(sorted(FIXTURES_PATH.glob('*.lisp')))
 UNPARSEABLE_FIXTURES = list(sorted(UNPARSEABLE_FIXTURES_PATH.glob('*.lisp')))
 
 
-def get_id(path: Path) -> str:
+def get_fixture_path_id(path: Path) -> str:
     return str(path.resolve())
 
 
 @pytest.fixture(
     params=FIXTURES,
-    ids=map(get_id, FIXTURES),
+    ids=get_fixture_path_id,
 )
 def parseable_lll_file(request):
     return request.param
@@ -26,7 +26,7 @@ def parseable_lll_file(request):
 
 @pytest.fixture(
     params=UNPARSEABLE_FIXTURES,
-    ids=map(get_id, UNPARSEABLE_FIXTURES),
+    ids=get_fixture_path_id,
 )
 def unparseable_lll_file(request):
     return request.param
